9c8a0c3d3ea54650e18581e50146ee97b79ccf75
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2024-06-27 16:18:48 -0700

    first post

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,15 +4,15 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Random Thoughts.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
+        A blog of random projects that I am working on and other stuff using{" "}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
-          Next.js
+          Next.js-Blog-Starter Kit
         </a>{" "}
         and {CMS_NAME}.
       </h4>
